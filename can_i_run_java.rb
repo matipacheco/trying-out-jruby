c8c9d70ddaed838a63b8ef3f5e87722820259783
Mati <matipacheco@gmail.com>
2019-02-13 00:07:29 -0300

    what the kind of hell is this. IT WORKSSSSSS

--- can_i_run_java.rb
+++ can_i_run_java.rb
@@ -1,10 +1,11 @@
 # https://github.com/jruby/jruby/wiki/CallingJavaFromJRuby
 
 require 'java'
+require '/Users/mati/Documents/trying-out-jruby-jar/out/artifacts/trying_out_jruby_jar_jar/trying-out-jruby-jar.jar'
 
 class CanIRunJava
   def self.can_i?
-    puts "Vamos a ver...."
+    Java::MysteriousFeature.viola()
   end
 end
 
